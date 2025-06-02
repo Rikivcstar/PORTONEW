521a577db43099dc6e49149978e4a42a60014af4
Rikivcstar <rikireza3r@gmail.com>
2025-06-02 22:36:21 +0700

    Debuuginnggg

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -12,7 +12,7 @@ export const Preloader = () => {
         <div role="status">
           <svg
             aria-hidden="true"
-            class="inline w-14 h-14 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
+            className="inline w-14 h-14 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
